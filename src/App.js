42cbadf93d9b88654dfc5be76fe426ca9d8320ed
Oleh Gerus <ogeru@softserveinc.com>
2022-03-07 19:18:44 +0200

    Add flatfile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FlatfileButton } from "@flatfile/react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlatfileButton
+      licenseKey="19efe851-9386-46cb-a999-3d7a62a77d71"
+      customer={{
+        companyId: "ABC-123",
+        companyName: "ABC Corp.",
+        email: "john@abc123.com",
+        name: "John Smith",
+        userId: "12345",
+      }}
+      settings={{
+        type: "Contact",
+        fields: [
+          { label: "Full Name", key: "name" },
+          { label: "Email", key: "email" },
+        ],
+        managed: true,
+        allowInvalidSubmit: false,
+      }}
+      onData={async (results) => {
+        return new Promise((resolve, reject) => {
+          reject({
+            message: "Error Message",
+            corrections: [
+              {
+                name: {
+                  value: "Foo Bar",
+                  info: [
+                    {
+                      message: "ERROR",
+                      level: "error",
+                    },
+                  ],
+                },
+              },
+              0,
+            ],
+          });
+        });
+      }}
+    >
+      Import Contacts
+    </FlatfileButton>
   );
-}
+};
 
 export default App;
